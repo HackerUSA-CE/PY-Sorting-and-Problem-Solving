49f1a0ed8fab2e6c8519eea3c600789c1f9be8f3
nkleinmann <nekleinmann@gmail.com>
2021-10-08 13:48:28 -0400

    Update starter code.

---
+++ bonus.py
@@ -0,0 +1,6 @@
+# Helpful hints:
+# Define a function named 'insertion_sort' that takes in an unsorted numerical list as an argument, 'test_list'.
+# Write a for loop that loops through the number of times that there are elements.
+# Set a variable called 'current_value' equal to the value at index 'i' in 'test_list' (note that 'i' here is the variable that is being incremented, if you used a different variable than use that in this step). This will be the element to be compared.
+# Compare the current element with the sorted portion. Write a while loop that runs while 'i' is greater than 0 and the value of the element of the list at index 'i-1' is greater than the current value.
+#     In the while loop, set the value of the element at index 'i' equal to the value of the element at index 'i-1'. Set 'i' equal to 'i-1'. Set the element at index 'i' equal to 'current_value'.
